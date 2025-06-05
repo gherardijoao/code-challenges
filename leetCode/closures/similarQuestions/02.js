f37881ea4bfd328058c76cdaa6255dac6e739c24
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-06-05 19:36:00 -0300

    structure change

---
+++ leetCode/closures/similarQuestions/02.js
@@ -0,0 +1,2 @@
+/*Create a function createSecret(value) that returns two functions: getSecret() and setSecret(newValue), and allows reading and changing the secret value — but only through the returned methods.*/
+
